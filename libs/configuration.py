f5e44177cd7700955aca85549c36a261c6d9baad
Javier Rodriguez Bouza <javier.rodriguezb@softtek.com>
2017-09-20 10:31:43 +0200

    Initial refactoring, not expected to work

--- libs/configuration.py
+++ libs/configuration.py
@@ -81,7 +81,7 @@ def configuration_parser(config_file):
     else:
         verbose = config["general"]["debug"]
     if config.has_option("general","interface"):
-        if config["general"]["interface"][:4] == 'wlan' or config["general"]["interface"][:3] == 'mon':
+        if config["general"]["interface"][:4] == 'wlan' or config["general"]["interface"][:3] == 'mon' or config["general"]["interface"][-3] == 'mon':
             intfmon = config["general"]["interface"]
         else:
             print config["general"]["interface"]
