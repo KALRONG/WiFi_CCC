c5fa728e96b7d9c92517485ca2c5500b0f7fbc30
Javier Rodriguez Bouza <javier.rodriguezb@softtek.com>
2017-09-20 23:28:46 +0200

    Initial refactoring, not expected to work

--- libs/files.py
+++ libs/files.py
@@ -3,27 +3,27 @@ import textwrap, base64
 
 ## filecrypt: function to split files in small parts and encrypt them
 def filecrypt(filename, chunksize):
-	try:
-		with open(filename, mode='rb') as payload:
-			fileContent = payload.read()
-	except:
-		fileContent=''
-		print ":chat: cannot open requested file: %s" %filename
-		return ''
-	try:
-		from configuration import cipher
-		parts = textwrap.wrap(fileContent, chunksize)
-		encoded_parts=set()
-		for part in parts:
-			lastpadd = len(part) % 16
-			if lastpadd > 0:
-				part = part + ("~" * (16 - lastpadd))
-			encoded_part = base64.b64encode(cipher.encrypt(part))
-			encoded_parts.add(encoded_part)
-		return encoded_parts
-	except Exception as e:
-		print ":chat: error disecting file: %s. %s" %(filename, e.message)
-		return ''
+    try:
+        with open(filename, mode='rb') as payload:
+            fileContent = base64.b64encode(payload.read())
+    except:
+        fileContent=''
+        print ":chat: cannot open requested file: %s" %filename
+        return ''
+    try:
+        from configuration import cipher
+        parts = textwrap.wrap(fileContent, chunksize)
+        encoded_parts=set()
+        for part in parts:
+            lastpadd = len(part) % 16
+        if lastpadd > 0:
+            part = part + ("~" * (16 - lastpadd))
+            encoded_part = base64.b64encode(cipher.encrypt(part))
+            encoded_parts.add(encoded_part)
+        return encoded_parts
+    except Exception as e:
+        print ":chat: error disecting file: %s. %s" %(filename, e.message)
+        return ''
 
 def remove_padding(message):
     i=len(message)-1
