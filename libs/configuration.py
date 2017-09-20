4bde65a152e73f64fcdebd6bb153751e40c26354
Javier Rodriguez Bouza <javier.rodriguezb@softtek.com>
2017-09-20 19:51:48 +0200

    Initial refactoring, not expected to work

--- libs/configuration.py
+++ libs/configuration.py
@@ -101,7 +101,7 @@ def configuration_parser(config_file):
     else:
         verbose = config["general"]["debug"]
     if config.has_option("general","interface"):
-        if config["general"]["interface"][:4] == 'wlan' or config["general"]["interface"][:3] == 'mon' or config["general"]["interface"][:3] == 'wlp':
+        if config["general"]["interface"][:4] == 'wlan' or config["general"]["interface"][:3] == 'mon' or config["general"]["interface"][:2] == 'wl':
             intfmon = str(config["general"]["interface"])
         else:
             print "Interface must be wlanx, monx or wlpx!"
