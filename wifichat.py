06c7849d673053f52e5dcf4b795bed75232ff65d
Javier Rodriguez Bouza <javier.rodriguezb@softtek.com>
2017-09-21 15:58:39 +0200

    Chat working, some features may not work as expected

--- wifichat.py
+++ wifichat.py
@@ -27,8 +27,7 @@ def main():
     utilization()
     from libs.configuration import privateircname
     try:
-        PacketProcessSend("%s joined the chat room: %s" % (username, privateircname))
-        #PacketProcessSend(":chat:%s joined the chat room: %s!" % (username, privateircname))  ## User entering group
+        PacketProcessSend("%s joined the chat room: %s" % (username, privateircname))  ## User entering group
         while 1:
             chat = raw_input()
             if chat != ":exit:":
