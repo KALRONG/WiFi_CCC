d57e27cf13759fdb511c2643f5e81736e8796f86
Javier Rodriguez Bouza <javier.rodriguezb@softtek.com>
2017-09-21 16:33:18 +0200

    Chat working, some features may not work as expected

--- libs/configuration.py
+++ libs/configuration.py
@@ -70,7 +70,7 @@ def create_cipher():
     from utils import channel_password
     global cipher
     iv = b"1234567890123456"
-    cipher = AES.new(channel_password(), AES.MODE_ECB)
+    cipher = AES.new(channel_password(), AES.MODE_CTR)
 
 def argument_parser():
     parser = argparse.ArgumentParser()
