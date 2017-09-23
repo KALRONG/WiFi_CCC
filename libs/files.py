80695db688e0265df4f5f3dcccfdd6ea0e78846f
Javier Rodriguez Bouza <javier.rodriguezb@softtek.com>
2017-09-23 11:25:57 +0200

    Chat working, some features may not work as expected

--- libs/files.py
+++ libs/files.py
@@ -17,6 +17,7 @@ def filecrypt(filename, chunksize):
         from configuration import cipher
         num_parts = str(len(textwrap.wrap(fileContent, chunksize, break_on_hyphens=False)))
         logger.debug("Number of parts: %s" % num_parts)
+        logger.debug("Chunksize: %s" % (chunksize - len(num_parts)))
         parts = textwrap.wrap(fileContent, chunksize-len(num_parts), break_on_hyphens=False)
         logger.debug("Parts: %s" % parts)
         encoded_parts=set()
